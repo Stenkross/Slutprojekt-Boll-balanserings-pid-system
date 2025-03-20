1385e1a7395308b8ff9f60a2683a0fc8d4518dcf
Stenkross <Tandkrok@gmail.com>
2025-03-20 13:37:28 +0100

    Pid system
    Har lagt till pidsystemet och dess andra värden

--- Slutprojekt/Slutprojekt.ino
+++ Slutprojekt/Slutprojekt.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Adafruit_VL53L0X.h"
 
+Adafruit_VL53L0X laser = Adafruit_VL53L0X();
+
+float p = 0, i = 0, d = 0, u = 0;
+
+float tp = 2.0;  
+float ti = 0.5; 
+float td = 2.0; 
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+  if (!laser.begin()) {
+    while (1)
+      ;
+  }
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  update();
+  delay(50);
+}
+
+int update() {
+  VL53L0X_RangingMeasurementData_t varde;
+  laser.rangingTest(&varde, false);
+  if (varde.RangeStatus != 4) {
+    Serial.println(varde.RangeMilliMeter);
+  }
+
+  d = (p - varde.RangeMilliMeter)/0.05;
+  p = varde.RangeMilliMeter;
+  i = 0.05 * p;
+
+
+  u = tp * (p + i / ti + td * d);
+  Serial.println(u);
 }
